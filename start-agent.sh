688f3179d6af55a7fc94f399a888d9b78863f023
1059690775 <1059690775@qq.com>
2018-05-25 14:06:24 +0800

    19

--- start-agent.sh
+++ start-agent.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ETCD_HOST=$(ip addr show docker0 | grep 'inet\b' | awk '{print $2}' | cut -d '/' -f 1)
+ETCD_HOST=etcd
 ETCD_PORT=2379
 ETCD_URL=http://$ETCD_HOST:$ETCD_PORT
 
@@ -12,7 +12,7 @@ if [[ "$1" == "consumer" ]]; then
        -Xms1536M \
        -Xmx1536M \
        -Dtype=consumer \
-       -Dserver.port=20000\
+       -Dserver.port=20000 \
        -Detcd.url=$ETCD_URL \
        -Dlogs.dir=/root/logs \
        /root/dists/mesh-agent.jar
@@ -22,9 +22,9 @@ elif [[ "$1" == "provider-small" ]]; then
        -Xms512M \
        -Xmx512M \
        -Dtype=provider \
-       -Dagent.server.serverCapacity=1 \
-       -Dserver.port=30000\
-       -Ddubbo.protocol.port=20889 \
+       -Ddubbo.protocol.port=20880 \
+       -Dagent.provider.serverCapacity=1\
+       -Dserver.port=30000 \
        -Detcd.url=$ETCD_URL \
        -Dlogs.dir=/root/logs \
        /root/dists/mesh-agent.jar
@@ -34,9 +34,9 @@ elif [[ "$1" == "provider-medium" ]]; then
        -Xms1536M \
        -Xmx1536M \
        -Dtype=provider \
-       -Dagent.provider.serverCapacity=2 \
-       -Dserver.port=30001\
-       -Ddubbo.protocol.port=20890 \
+       -Ddubbo.protocol.port=20880 \
+       -Dagent.provider.serverCapacity=2\
+       -Dserver.port=30000 \
        -Detcd.url=$ETCD_URL \
        -Dlogs.dir=/root/logs \
        /root/dists/mesh-agent.jar
@@ -46,9 +46,9 @@ elif [[ "$1" == "provider-large" ]]; then
        -Xms2560M \
        -Xmx2560M \
        -Dtype=provider \
-       -Dagent.server.serverCapacity=3 \
-       -Dserver.port=30002\
-       -Ddubbo.protocol.port=20891 \
+       -Ddubbo.protocol.port=20880 \
+       -Dagent.provider.serverCapacity=3\
+       -Dserver.port=30000 \
        -Detcd.url=$ETCD_URL \
        -Dlogs.dir=/root/logs \
        /root/dists/mesh-agent.jar
