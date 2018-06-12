4642150e197b92fefa753852b76667b5032747c1
1059690775 <1059690775@qq.com>
2018-06-12 11:07:52 +0800

    1

--- src/test/java/com/stouduo/agentmesh/AgentMeshApplicationTests.java
+++ src/test/java/com/stouduo/agentmesh/AgentMeshApplicationTests.java
@@ -2,15 +2,10 @@ package com.stouduo.agentmesh;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.web.reactive.function.client.WebClient;
-import reactor.core.publisher.Mono;
-
-import java.util.stream.Stream;
-
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@ContextConfiguration
 public class AgentMeshApplicationTests {
 
 
@@ -18,21 +13,5 @@ public class AgentMeshApplicationTests {
     public void contextLoads() {
     }
 
-    public static void main(String[] args) {
-        int retry = 3;
-        while (retry-- > 0) {
-            try {
-                test();
-            } catch (Exception e) {
-                if(retry==0)
-                    System.out.println("xxx");
-                else System.out.println(retry);
-            }
-        }
-    }
-
-    public static void test() throws Exception {
-        throw new IndexOutOfBoundsException("xxx");
-    }
 
 }
